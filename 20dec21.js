0ff8c3d287c87ea84ed07c64a8f6c65a082dafeb
AkshitaSagar <94096782+AkshitaSagar@users.noreply.github.com>
2021-12-29 11:55:48 +0530

    Add files via upload

---
+++ 20dec21.js
@@ -0,0 +1,34 @@
+//Q1. Write a program to check whether the number is greater than 10 or not.
+
+num=10;
+if(num>10)
+{
+console.log("num is greater than 10");
+}
+else if(num<10)
+{
+console.log("num is not greater than 10");
+}
+else
+{
+console.log("num is equal to 10");
+}
+
+//Q2. Write a program to have 3 variables (all three are number) and output the highest/greatest/maximum of all three.
+
+
+/*var n1=parseInt(prompt("enter 1st number"));
+var n2=parseInt(prompt("enter 2nd number"));
+var n3=parseInt(prompt("enter 3rd number"));
+if(n1>=n2,n1>=n3)
+{
+console.log(n1);
+}else if
+(n2>=n1,n2>=n3)
+console.log(n2);
+else
+{
+console.log(n3);
+}*/
+
+
